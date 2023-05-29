d453b8ea85a7c9d28c1cfb7737b0c26ec79f5e54
Franco_o <108852282+Franco122@users.noreply.github.com>
2023-05-29 18:21:05 -0300

    Update form.js

--- javascript/form.js
+++ javascript/form.js
@@ -4,32 +4,37 @@ const contrasenia = document.getElementById("contrasenia");
 const email = document.getElementById("email");
 const error = document.getElementById("error");
 
-function enviarError(){
-    var mensajeError = [];
-    if(nombre.value === null || nombre.value === ''){
-        mensajeError.push('Ingresa tu nombre');
-    }
-    if(apellido.value === null || apellido.value === ''){
-        mensajeError.push('Ingresa tu apellido');
-    }
-    if(contrasenia.value === null || contrasenia.value === ''){
-        mensajeError.push('Ingresa tu contraseña');
-    }
-    if(email.value === null || email.value === ''){
-        mensajeError.push('Ingresa tu email');
-    }
+function enviarError() {
+  var mensajeError = [];
 
+  if (nombre.value === null || nombre.value === '') {
+    mensajeError.push('Ingresa tu nombre');
+  }
+  if (apellido.value === null || apellido.value === '') {
+    mensajeError.push('Ingresa tu apellido');
+  }
+  if (contrasenia.value === null || contrasenia.value === '') {
+    mensajeError.push('Ingresa tu contraseña');
+  }
+  if (email.value === null || email.value === '') {
+    mensajeError.push('Ingresa tu email');
+  }
+
+  if (mensajeError.length > 0) {
     error.innerHTML = mensajeError.join("<br>");
-
-    // reseteo los inputs del form
+    return false;
+  } else {
+    // Reseteo los inputs del form
     document.getElementById("nombre").value = "";
     document.getElementById("apellido").value = "";
     document.getElementById("contrasenia").value = "";
     document.getElementById("email").value = "";
 
-    return false;
-
-    /* el unico error es que en el email puede enviar direcciones que no tengan sentido es porque no sabia si se podian usar expresiones regulares para validar con mas profundidad */
+    // Limpiar el mensaje de error
+    error.innerHTML = "";
+    return true;
+  }
 }
+
 
 
